2f00e8e356836652dfb4b41cb160b713b7cb9ef7
himanshuhkm <hackbrohkm@gmail.com>
2023-11-08 02:30:52 +0530

    2

---
+++ .next/static/OfpASr1EHzRMxv_zkdUqd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-27c18d15afc2ee37.js"],"/_error":["static/chunks/pages/_error-56a1a2eec59e6cbd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
